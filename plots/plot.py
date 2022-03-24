44fc422f80c09077d5881d049bc3686cb968e72d
Jakob Hedemann <jhammerh92@gmail.com>
2022-03-24 13:00:42 +0100

    added plot_slider function

--- plots/plot.py
+++ plots/plot.py
@@ -3,6 +3,7 @@ from os import listdir
 import matplotlib.pyplot as plt
 from  mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from mpl_toolkits import mplot3d
+from matplotlib.widgets import Slider, Button
 import numpy as np
 import skimage.io
 import scipy as sp
@@ -65,3 +66,75 @@ def plot_im_as_surf(im, ax=None, **kwargs):
         
     ax = plt.axes(projection='3d')
     ax.plot_surface(X, Y, im, **kwargs)
+
+
+
+
+
+
+# creates a plot with sliders, one or two, using a callback function eg.:
+"""
+def callback(slider1, slider2, **params):
+    x = np.linspace(-10,10)
+    return slider1*x**2  + slider2*x + c
+"""
+# and us the function with the callback, like this:
+"""
+plot_slider(callback, [[-2,0,2],[-2,0,2]], c=2)
+
+"""
+def plot_slider(callback, sliders, plot_type="plot", **callback_params):
+    fig, ax = plt.subplots()
+    if len(sliders) == 2:
+        out = callback(sliders[0][1], sliders[1][1], **callback_params)
+    else:
+        out = callback(sliders[0][1], **callback_params)
+
+    if plot_type == "plot":
+        x = np.arange(0, len(out))
+        plot, = ax.plot(x, out)
+    elif plot_type == "image":
+        plot = ax.imshow(out)
+    plt.subplots_adjust(left=0.25, bottom=0.25)
+
+
+    ax_slider1 = plt.axes([0.25, 0.1, 0.65, 0.03])
+    slider_1 = Slider(
+        ax=ax_slider1,
+        label='Slider 1',
+        valmin=sliders[0][0],
+        valmax=sliders[0][2],
+        valinit=sliders[0][1],
+    )
+
+    if len(sliders)==2: # define second slider
+        ax_slider2 = plt.axes([0.1, 0.25, 0.0225, 0.63])
+        slider_2 = Slider(
+            ax=ax_slider2,
+            label='Slider 2',
+            valmin=sliders[1][0],
+            valmax=sliders[1][2],
+            valinit=sliders[1][1],
+            orientation='vertical'
+        )
+
+
+    def update(val):
+        if plot_type == "plot":
+            if 'slider_2' in locals():
+                plot.set_data(x, callback(slider_1.val, slider_2.val, **callback_params))
+            else:
+                plot.set_data(x, callback(slider_1.val, **callback_params))
+        elif plot_type == "image":
+            if 'slider_2' in locals():
+                plot.set_data(callback(slider_1.val, slider_2.val, **callback_params))
+            else:
+                plot.set_data(callback(slider_1.val, **callback_params))
+        ax.relim()
+        ax.autoscale_view(True,True,True)
+        fig.canvas.draw_idle()
+
+    slider_1.on_changed(update)
+    if 'slider_2' in locals(): slider_2.on_changed(update)
+
+    plt.show()
